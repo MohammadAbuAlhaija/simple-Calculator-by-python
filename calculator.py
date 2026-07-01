cbf0bde24275531c4cfb2718fd0698cb94774e72
Mohammad Abu Alhaija <eng.mohammadabualhija@gmail.com>
2026-07-01 17:19:02 +0300

    Add basic calculator functionality

--- calculator.py
+++ calculator.py
@@ -1,1 +1,16 @@
 
+num1=float(input("First Number: "))
+num2=float(input("Second Number: "))
+
+operation=input("Cloose(+,-,*,/):")
+
+if operation =="+":
+    print(f"Result: {num1 + num2}")
+elif operation =="-":
+    print(f"Result: {num1 - num2}")
+elif operation =="*":
+    print(f"Result: {num1 * num2}")
+elif operation =="/":
+    print(f"Result: {num1 / num2}")
+else:
+    print("Invalid operation")
